b6b5a64bf444ab6f9d303041d6ee82b0649f81f5
Jeremiah Oriyomi <jeremiahoriyomi2000@gmail.com>
2024-11-15 23:02:54 -0800

    Worked on Javascript project

--- asynchronous3.js
+++ asynchronous3.js
@@ -15,8 +15,25 @@ async function displayUserData() {
         userList.innerHTML = listItems;
     }catch (error) {
         console.log('error fetching data:', error)
-        userList.innerHTML = `<li> Error Fetching User data. please try angain later. </li>`
+        userList.innerHTML = `<li> Error Fetching User data. please try again later. </li>`  // Display an error message in the user-list
     }
 }
 
-displayUserData()
+// Select the 'view' button element
+const viewButton = document.getElementById('view');
+// Select the 'hide' button element
+const hideButton = document.getElementById('hide');
+// Select the user list container element
+const userList = document.getElementById('user-list');
+
+
+// Add a click event listener to the 'view' button
+viewButton.addEventListener('click', () => {  // Make the the user list container visible
+    userList.style.display = 'block'; 
+    displayUserData(); // call the function to display user data
+});
+
+// Add a click event listener to the 'hide' button
+hideButton.addEventListener('click', () => { // Hide the user list container
+    userList.style.display = 'none'; 
+});
